7caa07345e0888f08d6c54e435d7b1dc22a70472
Nagma Bardoi <nagmabardoi@gmail.com>
2019-11-12 21:28:52 -0600

    display pictures and text from response

--- restaurant.js
+++ restaurant.js
@@ -9,7 +9,7 @@ $("#search").on("click", function() {
         
         $("#search-input").val("");
     
-        var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + search + "&lat=" + lat + "&lon=" + lon;
+        var queryURL = "https://developers.zomato.com/api/v2.1/search?q=" + search + "&lat=" + lat + "&lon=" + lon + "&count=12";
 
         $.ajax({
             url: queryURL,
@@ -21,6 +21,31 @@ $("#search").on("click", function() {
             contentType: 'application/json; charset=utf-8',
         })   .then(function(response) {
                 console.log(response);
+
+                var results = response.restaurants;
+                
+                for (var i = 0; i < results.length; i++) {
+                var columnDiv = $("<div>").addClass("column");
+                var calloutDiv = $("<div>").addClass("callout");
+                var rest_name = $("<p>").text(results[i].restaurant.name).addClass("rest_name");
+                var cuisines = $("<p>").text("Cuisines: " + results[i].restaurant.cuisines).addClass("rest_name");
+                var rest_Image = $("<img>");
+
+                var photo_URL = "";
+                if (results[i].restaurant.photo_count == "0") {
+                    photo_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWMqnevv6WK35syKtSA6dLfOnZxTQ0COwnNw6PskHtkgBtkxaO&s";
+                    rest_Image.attr("alt", "NO PHOTOS FOUND");
+                } else {
+                    photo_URL = results[i].restaurant.photos[0].photo.url
+                };
+
+                rest_Image.attr("src", photo_URL);
+                columnDiv.append(calloutDiv);
+                calloutDiv.append(rest_name);
+                calloutDiv.append(rest_Image);
+                calloutDiv.append(cuisines);
+                $("#restaurants-div").append(columnDiv);
+                }
         });
 
 });
