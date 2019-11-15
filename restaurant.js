5a9ded40d0d40f26a6a81e326205560456d605b6
Nagma Bardoi <nagmabardoi@gmail.com>
2019-11-14 19:50:02 -0600

    feast page works and all info show up

--- restaurant.js
+++ restaurant.js
@@ -1,8 +1,23 @@
-var lat = latResults;
-var lon = lngResults;
+$(".lead").hide();
+
+var lat = "";
+var lon = "";
+
+database.ref().on("child_added", function(snapshot) {
+
+    var sv = snapshot.val();
+
+    lat = sv.latResults
+    lon = sv.lngResults
+
+    console.log(lat);
+    console.log(lon);
+});
 
 $("#search").on("click", function() {
     event.preventDefault();
+
+    $("#restaurants-div").empty();
 
         var search = $("#search-input").val().trim();
         console.log(search);
@@ -25,27 +40,50 @@ $("#search").on("click", function() {
                 var results = response.restaurants;
                 
                 for (var i = 0; i < results.length; i++) {
+                $(".lead").show();
                 var columnDiv = $("<div>").addClass("column");
                 var calloutDiv = $("<div>").addClass("callout");
                 var rest_name = $("<p>").text(results[i].restaurant.name).addClass("rest_name");
-                var cuisines = $("<p>").text("Cuisines: " + results[i].restaurant.cuisines).addClass("rest_name");
+                var cuisines = $("<p>").text("Cuisines: " + results[i].restaurant.cuisines).addClass("cuisines");
+                var costForTwo = $("<p>").text("Cost for Two: $" + results[i].restaurant.average_cost_for_two).addClass("costForTwo");
+                var timings = $("<p>").text("Timings: " + results[i].restaurant.timings).addClass("timings");
+                var phone_Number = $("<p>").text("Phone Number: " + results[i].restaurant.phone_numbers).addClass("phone_Number");
+                var rating = $("<p>").text("Rating: " + results[i].restaurant.user_rating.aggregate_rating).addClass("phone_Number");
+                var ratingText = $("<p>").text(results[i].restaurant.user_rating.rating_text).addClass("phone_Number");
+
+                var address = $("<p>").text("Address: " + results[i].restaurant.location.address).addClass("address");
                 var rest_Image = $("<img>");
 
                 var photo_URL = "";
+
                 if (results[i].restaurant.photo_count == "0") {
-                    photo_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWMqnevv6WK35syKtSA6dLfOnZxTQ0COwnNw6PskHtkgBtkxaO&s";
+                    photo_URL = "https://b.zmtcdn.com/images/placeholder_200_v2.jpg?fit=around%7C375%3A277&crop=375%3A277%3B%2A%2C%2A";
                     rest_Image.attr("alt", "NO PHOTOS FOUND");
-                } else {
+                }else {
                     photo_URL = results[i].restaurant.photos[0].photo.url
                 };
-
+                
                 rest_Image.attr("src", photo_URL);
                 columnDiv.append(calloutDiv);
                 calloutDiv.append(rest_name);
+                calloutDiv.append(rating);
+                calloutDiv.append(ratingText);
                 calloutDiv.append(rest_Image);
                 calloutDiv.append(cuisines);
+                calloutDiv.append(costForTwo);
+                calloutDiv.append(address);
+                calloutDiv.append(timings);
+                calloutDiv.append(phone_Number);
+
+
                 $("#restaurants-div").append(columnDiv);
                 }
         });
 
 });
+
+$("#zip").on("click", function() {
+    event.preventDefault();
+
+    window.location = "index.html";
+});   
