4b49cbf4580c21b6abd4f068d06a5b6c8b18845a
Marcos Luna <ml042685@yahoo.com>
2019-11-14 21:12:18 -0600

    events_page.js2

--- events_page.js
+++ events_page.js
@@ -1,5 +1,30 @@
-var lat = latResults;
-var lon = lngResults;
+// var zip = "";
+var zip = "";
+
+
+// database.ref().on("child_added", function(snapshot) {
+    
+//     var sv = snapshot.val();
+//     zip = sv.zip;
+//     console.log(zip);
+
+// });
+var events = [""];
+var genre = [""];
+var start = [""];
+var images = [""];
+
+
+
+// append multiple values to the array
+events.push();
+
+// display all values
+for (var i = 0; i < events.length; i++) {
+    
+  console.log(events[i]);
+}
+
 
 $("#search").on("click", function() {
     event.preventDefault();
@@ -9,18 +34,23 @@ $("#search").on("click", function() {
         
         $("#search-input").val("");
     
-        var queryURL = "" + search + "&lat=" + lat + "&lon=" + lon;
+        // var apiKeyTM = ML6a8XQhSfo7pMxWI2w4XrqUeERBnspS
+        var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?size=10&apikey=ML6a8XQhSfo7pMxWI2w4XrqUeERBnspS&postalCode=" + zip;
 
-        $.ajax({
+
+        $.ajax ({
+            type:"GET",
             url: queryURL,
-            type: 'GET',
-            dataType: 'json',
-            headers: {
-                'user-key': '',
-            },
-            contentType: 'application/json; charset=utf-8',
-        })   .then(function(response) {
-                console.log(response);
-        });
+            async:true,
+            dataType: "json",
+            success: function(json) {
+                        console.log(json);
+                    }
+                });
 
-});
+                     
+            // error: function (xhr, status, err) {
+
+            // }
+                        // This time, we do not end up here!
+                 });
