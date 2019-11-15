4e1364112c05da2797f4ba40f07c7cd5d1f1e56e
nbardoi <54256414+nbardoi@users.noreply.github.com>
2019-11-14 20:33:56 -0600

    Revert "Update landing.js"

--- landing.js
+++ landing.js
@@ -1,21 +1,19 @@
 var firebaseConfig = {
     apiKey: "AIzaSyARc2BoDAwXfBOGquZA2fajsAOeOLp8CTg",
-    authDomain: "new-member-a5313.firebaseapp.com",
+    authDomain: "new-member - a5313.firebaseapp.com",
     databaseURL: "https://new-member-a5313.firebaseio.com",
-    projectId: "new-member-a5313",
-    storageBucket: "new-member-a5313.appspot.com",
+      projectId: "new-member - a5313",
+    storageBucket: "new-member - a5313.appspot.com",
     messagingSenderId: "400495999207",
-    appId: "1:400495999207:web:9ea3589de3114bb4694a42",
-    measurementId: "G-XBZFDJJ4K0"
+    appId: "1: 400495999207: web: 9ea3589de3114bb4694a42",
+    measurementId: "G- XBZFDJJ4K0"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   var database = firebase.database();
-
   var zip;
   var latResults = 0;
   var lngResults = 0;
-
   $("button").on("click", function () {
     event.preventDefault();
     
@@ -36,13 +34,11 @@ var firebaseConfig = {
         lngResults = response.results["0"].locations["0"].latLng.lng;
         console.log(lngResults);
         console.log(latResults);
-        
         database.ref().push({
         zip: zip,
         latResults: latResults,
         lngResults: lngResults
         });
-        
         window.location = "restaurant.html";
     });
   });
