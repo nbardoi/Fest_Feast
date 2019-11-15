323e3a3f6703a2927cd6dd996107ae28b6e79794
James Goff <james_goff88@outlook.com>
2019-11-14 20:24:23 -0600

    add second button and button click

--- landing.js
+++ landing.js
@@ -8,38 +8,65 @@ var firebaseConfig = {
     appId: "1: 400495999207: web: 9ea3589de3114bb4694a42",
     measurementId: "G- XBZFDJJ4K0"
   };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
-  var zip;
-  var latResults = 0;
-  var lngResults = 0;
-  $("button").on("click", function () {
-    event.preventDefault();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var zip;
+var latResults = 0;
+var lngResults = 0;
+$("#feast-search").on("click", function () {
+  event.preventDefault();
     
-    //get number value from input
-    zip = $("#zip - input").val();
-    console.log(zip);
-    $("#zip - input").val("");
+  //get number value from input
+  zip = $("#zip - input").val();
+  console.log(zip);
+  $("#zip - input").val("");
   
-    var queryURL = "https://www.mapquestapi.com/geocoding/v1/address?key=OBoNOwnKwrAtuAq41mWwlOIvwGOj1mSV&inFormat=kvp&outFormat=json&location=" + zip + "&thumbMaps=false";
-      // Performing an AJAX request with the queryURL
-    $.ajax({
-    url: queryURL,
-    method: "GET"
-    })
-    .then(function (response) {
-        //console.log(response);
-        latResults = response.results["0"].locations["0"].latLng.lat;
-        lngResults = response.results["0"].locations["0"].latLng.lng;
-        console.log(lngResults);
-        console.log(latResults);
-        database.ref().push({
-        zip: zip,
-        latResults: latResults,
-        lngResults: lngResults
-        });
-        window.location = "restaurant.html";
+  var queryURL = "https://www.mapquestapi.com/geocoding/v1/address?key=OBoNOwnKwrAtuAq41mWwlOIvwGOj1mSV&inFormat=kvp&outFormat=json&location=" + zip + "&thumbMaps=false";
+    // Performing an AJAX request with the queryURL
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+  })
+  .then(function (response) {
+    //console.log(response);
+    latResults = response.results["0"].locations["0"].latLng.lat;
+    lngResults = response.results["0"].locations["0"].latLng.lng;
+    console.log(lngResults);
+    console.log(latResults);
+    database.ref().push({
+      zip: zip,
+      latResults: latResults,
+      lngResults: lngResults
     });
+    window.location = "restaurant.html";
   });
-  
+});
+$("#fest-search").on("click", function () {
+  event.preventDefault();
+    
+  //get number value from input
+  zip = $("#zip - input").val();
+  console.log(zip);
+  $("#zip - input").val("");
+
+  var queryURL = "https://www.mapquestapi.com/geocoding/v1/address?key=OBoNOwnKwrAtuAq41mWwlOIvwGOj1mSV&inFormat=kvp&outFormat=json&location=" + zip + "&thumbMaps=false";
+    // Performing an AJAX request with the queryURL
+  $.ajax({
+  url: queryURL,
+  method: "GET"
+  })
+  .then(function (response) {
+    //console.log(response);
+    latResults = response.results["0"].locations["0"].latLng.lat;
+    lngResults = response.results["0"].locations["0"].latLng.lng;
+    console.log(lngResults);
+    console.log(latResults);
+    database.ref().push({
+      zip: zip,
+      latResults: latResults,
+      lngResults: lngResults
+    });
+    window.location = "events_page.html";
+  });
+});
